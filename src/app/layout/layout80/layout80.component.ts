787b75ada9fd0e16d2e3426709b4521cee4a8aad
jay sharma <jksharma7330@gmail.com>
2019-04-28 17:36:03 +0530

    product by category template integrated

--- src/app/layout/layout80/layout80.component.ts
+++ src/app/layout/layout80/layout80.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-layout80',
   templateUrl: './layout80.component.html',
-  styleUrls: ['./layout80.component.css']
+  styleUrls: ['./layout80.component.css'],
 })
 export class Layout80Component implements OnInit {
 
--- src/app/product-banner-category/product-banner-category.component.ts
+++ src/app/product-banner-category/product-banner-category.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-product-banner-category',
   templateUrl: './product-banner-category.component.html',
-  styleUrls: ['./product-banner-category.component.css']
+  styleUrls: ['./product-banner-category.component.css'],
+
 })
 export class ProductBannerCategoryComponent implements OnInit {
 
