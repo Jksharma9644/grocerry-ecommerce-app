1ec5d4dffbc607be4b3201f9ec403248bbb29959
jay sharma <jksharma7330@gmail.com>
2019-04-29 15:19:33 +0530

    configuration changes

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+// const app = express();
+
+// const forceSSL = function() {
+//     return function (req, res, next) {
+//       if (req.headers['x-forwarded-proto'] !== 'https') {
+//         return res.redirect(
+//          ['https://', req.get('Host'), req.url].join('')
+//         );
+//       }
+//       next();
+//     }
+//   }
+
+// app.use(forceSSL());
+
+
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+//   });
+
+// // Run the app by serving the static files
+// // in the dist directory
+// app.use(express.static(__dirname + '/dist'));
+
+// // Start the app by listening on the default
+// // Heroku port
+// app.listen(process.env.PORT || 5200);const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/grocerry-ecommerce-app'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/grocerry-ecommerce-app/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5200);
