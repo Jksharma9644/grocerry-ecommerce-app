39d7eaa536081d64a5adecd36c5447b126dfc034
jay sharma <jksharma7330@gmail.com>
2019-05-26 17:35:39 +0530

    email and otp verification

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
-type UserFields = 'email' | 'password' | 'username' | 'mobile';
+import { Router, CanActivate } from '@angular/router';
+
+type UserFields = 'email' | 'password' | 'username' | 'mobile' | 'otp';
 type FormErrors = { [u in UserFields]: string };
 
 @Component({
@@ -13,15 +15,21 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   registerForm: FormGroup;
+  otpVerificationForm: FormGroup;
+
   public action = 'Register';
-  public message="";
-  public regComp:any=null;
+  public message = "";
+  public regComp: any = null;
+  public loginResponse = "";
+  public userInfo: any;
+  public otpVerification: any = null;
 
   formErrors: FormErrors = {
     'email': '',
     'password': '',
     'username': '',
-    'mobile': ''
+    'mobile': '',
+    'otp': ''
   };
   validationMessages = {
     'email': {
@@ -42,17 +50,24 @@ export class LoginComponent implements OnInit {
       'required': 'mobile no is required',
       'pattern': 'mobile no is not in correct format',
 
+    },
+    'otp': {
+      'required': 'enter otp to verify mobile no',
     }
+
   };
 
-  constructor(private fb: FormBuilder, public _authService: AuthService) { }
+  constructor(private router: Router, private fb: FormBuilder, public _authService: AuthService) { }
 
   ngOnInit() {
     this.buildForm();
   }
   buildForm() {
     this.loginForm = this.fb.group({
-      'username': new FormControl('', Validators.required),
+      'email': new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
       'password': new FormControl('', [
         Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$'),
         Validators.minLength(6),
@@ -79,15 +94,25 @@ export class LoginComponent implements OnInit {
         Validators.required
       ])
     });
-    this.registerForm.valueChanges.subscribe((data) => this.onValueChanged(data));
-    this.onValueChanged();
-  }
+    this.otpVerificationForm = this.fb.group({
+      'otp': new FormControl('', [
+        Validators.required,
+      ]),
 
-  onValueChanged(data?: any) {
-    if (!this.registerForm) { return; }
-    const form = this.registerForm;
+    })
+    this.registerForm.valueChanges.subscribe((data) => this.onValueChanged(data));
+    this.loginForm.valueChanges.subscribe((data) => this.onValueChanged2(data));
+    this.onValueChanged();
+    this.onValueChanged2();
+
+
+  }
+  onValueChanged2(data?: any) {
+    this.message = "";
+    if (!this.loginForm) { return; }
+    const form = this.loginForm;
     for (const field in this.formErrors) {
-      if (Object.prototype.hasOwnProperty.call(this.formErrors, field) && (field === 'email' || field === 'password' || field === 'username'  || field === 'mobile')) {
+      if (Object.prototype.hasOwnProperty.call(this.formErrors, field) && (field === 'email' || field === 'password' || field === 'username' || field === 'mobile')) {
         // clear previous error message (if any)
         this.formErrors[field] = '';
         const control = form.get(field);
@@ -104,35 +129,107 @@ export class LoginComponent implements OnInit {
       }
     }
   }
-  toggle(flag){
-   this.login=flag;
-   this.registerForm.reset();
-   this.loginForm.reset();
+
+  onValueChanged(data?: any) {
+    this.message = "";
+    if (!this.registerForm) { return; }
+    const form = this.registerForm;
+    for (const field in this.formErrors) {
+      if (Object.prototype.hasOwnProperty.call(this.formErrors, field) && (field === 'email' || field === 'password' || field === 'username' || field === 'mobile')) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          if (control.errors) {
+            for (const key in control.errors) {
+              if (Object.prototype.hasOwnProperty.call(control.errors, key)) {
+                this.formErrors[field] += `${(messages as { [key: string]: string })[key]} `;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  toggle(flag) {
+    this.login = flag;
+    this.registerForm.reset();
+    this.loginForm.reset();
   }
 
   login() {
+    this.message = "Please Wait....";
+    var request = this.loginForm.value;
+    this._authService.LoginwithAPI(request).subscribe(res => {
+      var response = res;
+      console.log(response);
+      if (response["status"]) {
+        this.loginResponse = response["data"];
+        localStorage.setItem('token', JSON.stringify({ user: this.loginResponse, token: this.loginResponse["token"] }));
+        this._authService.authenticatedUser.user_id = this.loginResponse["user_id"];
+        this._authService.authenticatedUser.name = this.loginResponse["name"];
+        this._authService.authenticatedUser.email = this.loginResponse["email"];
+        this._authService.authenticatedUser.token = this.loginResponse["token"];
+        this.message = "";
+        this.router.navigate(['']);
+
+      } else {
+        this.message = response["message"];
+      }
+
+    })
 
   }
   Register() {
     var request = this.registerForm.value;
     console.log(request);
-    this.regComp=false;
-    this.message="Please wait while we are processing your request ....";
+    this.regComp = false;
+    this.message = "Please wait while we are processing your request ....";
     this._authService.SignUpwithAPI(request).subscribe(res => {
-      this.message="";
-        if(res["status"]){
-          this.regComp=true;
-        }else{
-          setTimeout(()=>{
-            this.regComp=null;
-          },500)
-        }
-        this.message=res["message"];
-    
+      this.message = "";
+      if (res["status"]) {
+        this.regComp = true;
+        this.userInfo = res["data"];
+        console.log(this.userInfo);
+      } else {
+        setTimeout(() => {
+          this.regComp = null;
+        }, 500)
+      }
     })
   }
-  ngOnDestroy(){
-    this.regComp=null;
+  verifyOtp() {
+    this.message = "Please Wait";
+    this.otpVerification = false;
+    var request = { user: this.userInfo, otp: this.otpVerificationForm.value.otp };
+    this._authService.verifyOTP(request).subscribe(res => {
+      console.log("otp verification", res);
+      if (res["status"]) {
+        this.otpVerification = true;
+        this.otpVerificationForm.reset();
+        this.registerForm.reset();
+        this.message = res["message"];
+      }
+
+
+    })
+  }
+  sendOTP() {
+    var request = this.userInfo;
+    this._authService.sendVerificationOTP(request).subscribe(res => {
+      console.log("send otp", res)
+      if (res["status"]) {
+        this.message = res["message"];
+      }
+
+
+
+
+    })
+  }
+  ngOnDestroy() {
+    this.regComp = null;
   }
 
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Router, CanActivate } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,39 +11,61 @@ import { environment } from '../../environments/environment';
 export class AuthService {
   public headers: any;
   public baseURl: any;
+  public authenticatedUser: any = {
+    user_id: "",
+    token: "",
+    name: "",
+    email: "",
+  };
 
-  constructor(public jwtHelper: JwtHelperService, public http: HttpClient) {
+  constructor(public jwtHelper: JwtHelperService, public http: HttpClient, private router: Router) {
     this.baseURl = environment.apiUrl;
   }
 
 
 
-  isAuthenticated(): boolean {
-    const token = localStorage.getItem('token');
 
-    return !this.jwtHelper.isTokenExpired(token);
+  isAuthenticated(): boolean {
+    if (localStorage.getItem('token')) {
+      // logged in so return true
+      return true;
+    }
+
+    return false;
+
 
   }
   SignUpwithAPI(req) {
     const headers = new HttpHeaders()
       .set("Content-Type", "application/json");
-    return this.http.post(this.baseURl + 'auth/register',req, { headers })
+    return this.http.post(this.baseURl + 'auth/register', req, { headers })
   }
 
   LoginwithAPI(req) {
     const headers = new HttpHeaders()
-    .set("Content-Type", "application/json");
+      .set("Content-Type", "application/json");
     return this.http.post(this.baseURl + 'auth/sign_in', req, { headers })
 
   }
   AuthenticateUrl(req) {
     const headers = new HttpHeaders()
-    .set("Content-Type", "application/json");
-    return this.http.post(this.baseURl + 'auth/confirmation',req, { headers });
+      .set("Content-Type", "application/json");
+    return this.http.post(this.baseURl + 'auth/confirmation', req, { headers });
+  }
+  sendVerificationOTP(req) {
+    const headers = new HttpHeaders()
+      .set("Content-Type", "application/json");
+    return this.http.post(this.baseURl + 'auth/sendOTP', req, { headers });
   }
 
-  logout(){
+  verifyOTP(req) {
+    const headers = new HttpHeaders()
+      .set("Content-Type", "application/json");
+    return this.http.post(this.baseURl + 'auth/verifyOTP', req, { headers });
+  }
 
+  logout() {
+    localStorage.removeItem("token");
   }
 
 }
